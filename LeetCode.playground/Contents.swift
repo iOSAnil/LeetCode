92d751d924ee59ded1c279fc49cec0355f5b8be2
AnilKothari <anilkothari6588@gmail.com>
2024-01-24 11:16:04 +0530

    1768. Merge Strings Alternately 11. Container With Most Water

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -34,7 +34,6 @@ class MaximumDepthOfBinaryTree {
         }
      }
     
-    
     func arrayToTree(_ arr: [Int?], _ index: Int) -> TreeNode? {
         guard index < arr.count else {
             return nil
@@ -56,3 +55,61 @@ let tree = MaximumDepthOfBinaryTree().arrayToTree([3,9,20,nil,nil,15,7], 0)
 let treeDepth = MaximumDepthOfBinaryTree().maxDepth(tree)
 
 // -------------------------------------------------------------------------------------------------------------------------
+
+
+// ---------------------------------1768. Merge Strings Alternately ------------------------------------------------------
+
+class MergeStringAlternately {
+    func mergeAlternately(_ word1: String, _ word2: String) -> String {
+        let word1Array = Array(word1)
+        let word2Array = Array(word2)
+        let big = max(word1Array.count, word2Array.count)
+        var result = [Character]()
+        for i in 0..<big {
+            if i < word1.count {
+                result.append(word1Array[i])
+            }
+            if i < word2.count {
+                result.append(word2Array[i])
+                
+            }
+        }
+        return String(result)
+    }
+}
+    
+let mergeAlternately = MergeStringAlternately().mergeAlternately("abc", "pqr")
+print(mergeAlternately)
+// Note - String.count method takes longer in ms than an Array(string).count
+// -------------------------------------------------------------------------------------------------------------------------
+
+
+
+// ---------------------------------11. Container With Most Water------------------------------------------------------
+
+class ContainerWithMostWater {
+    func maxArea(_ height: [Int]) -> Int {
+        var area = 0
+        var left = 0
+        var right = height.count - 1
+        
+        while left < right {
+            let leftHeight = height[left]
+            let rightHeight = height[right]
+            let currentArea = min(leftHeight, rightHeight)*(right-left)
+            area = max(area, currentArea)
+            
+            if leftHeight > rightHeight {
+                right -= 1
+            } else {
+                left += 1
+            }
+        }
+        return area
+    }
+}
+
+print(ContainerWithMostWater().maxArea([1,8,6,2,5,4,8,3,7]))
+// -------------------------------------------------------------------------------------------------------------------------
+
+
