5336d41aa387a5b19aa8c69bebc420897bba1de6
AnilKothari <anilkothari6588@gmail.com>
2024-06-30 22:36:33 +0530

    21. Merge two sorted lists

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -14,6 +14,16 @@ public class TreeNode {
     }
 }
 
+//Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
 // ---------------------------------104. Maximum Depth of Binary Tree------------------------------------------------------
 class MaximumDepthOfBinaryTree {
     func maxDepth(_ root: TreeNode?) -> Int {
@@ -1368,4 +1378,37 @@ let three = TreeNode(3)
 let root = TreeNode(1, two, three)
 print(PathSum().hasPathSum(root, 3))
 
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------21. Merge Two Sorted Lists----------------------------------------------------------
+
+func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+    // Create a dummy node to simplify the merging process
+    let dummy = ListNode(0)
+    var current: ListNode? = dummy
+    
+    // Pointers to traverse the lists
+    var l1 = list1
+    var l2 = list2
+    
+    while (l1 != nil && l2 != nil) {
+        if l1!.val <= l2!.val {
+            current?.next = l1
+            l1 = l1?.next
+        } else {
+            current?.next = l2
+            l2 = l2?.next
+        }
+        
+        current = current?.next
+    }
+    
+    if l1 != nil {
+        current?.next = l1
+    } else if l2 != nil {
+        current?.next = l2
+    }
+    
+    return dummy.next
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
