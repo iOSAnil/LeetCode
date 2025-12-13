c3583c2a0f465a916035c910860437eca6c2caaf
AnilKothari <anilkothari6588@gmail.com>
2025-12-13 13:22:52 +0530

    27. Remove Element

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -3066,3 +3066,24 @@ func convert(_ s: String, _ numRows: Int) -> String {
 }
 print(convert("PAYPALISHIRING", 3))
 // ------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------27. Remove Element------------------------------------------------------------------------------------
+/*
+ Input: nums = [0,1,2,2,3,0,4,2], val = 2
+ Output: 5, nums = [0,1,4,0,3,_,_,_]
+ Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+ Note that the five elements can be returned in any order.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    var k = 0
+    for i in 0..<nums.count {
+        if nums[i] != val {
+            nums[k] = nums[i]
+            k += 1
+        }
+    }
+    return k
+}
+// ------------------------------------------------------------------------------------------------------------------------------
+
